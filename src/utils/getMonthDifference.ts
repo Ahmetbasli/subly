0b94370b9545439f3e7481117d62ef6713a0fb9f
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-07-14 22:46:00 +0300

    Add date difference utility func

---
+++ src/utils/getMonthDifference.ts
@@ -0,0 +1,8 @@
+const getMonthDifference = (startDate: Date, endDate: Date) => {
+  return (
+    endDate.getMonth() -
+    startDate.getMonth() +
+    12 * (endDate.getFullYear() - startDate.getFullYear())
+  );
+};
+export default getMonthDifference;
